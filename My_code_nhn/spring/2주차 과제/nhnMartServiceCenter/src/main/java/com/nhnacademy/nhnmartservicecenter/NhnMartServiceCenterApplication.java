85ee9e881ab272af0ceb7edb4fe5eb9d03bce716
권용민 <kwonym0914@gmail.com>
2025-04-10 09:50:23 +0900

    [feat] : project setting

---
+++ My_code_nhn/spring/2주차 과제/nhnMartServiceCenter/src/main/java/com/nhnacademy/nhnmartservicecenter/NhnMartServiceCenterApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.nhnmartservicecenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhnMartServiceCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NhnMartServiceCenterApplication.class, args);
+    }
+
+}
---
+++ My_code_nhn/spring/2주차 과제/nhnMartServiceCenter/src/test/java/com/nhnacademy/nhnmartservicecenter/NhnMartServiceCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.nhnmartservicecenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhnMartServiceCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
