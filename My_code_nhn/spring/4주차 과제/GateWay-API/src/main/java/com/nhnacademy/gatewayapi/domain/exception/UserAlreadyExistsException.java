6873061304953223be8031b9e7634b497bbc751f
권용민 <kwonym0914@gmail.com>
2025-05-02 10:19:04 +0900

    Revert "로그인 구현 완료"
    This reverts commit c6b777b6c32df7089fc84acfc7382161183f6b51.

--- My_code_nhn/spring/4주차 과제/GateWay-API/src/main/java/com/nhnacademy/gatewayapi/domain/exception/UserAlreadyExistsException.java
+++
@@ -1,5 +0,0 @@
-package com.nhnacademy.gatewayapi.domain.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-
-}
--- My_code_nhn/spring/4주차 과제/GateWay-API/src/main/java/com/nhnacademy/gatewayapi/domain/exception/UserNotFoundException.java
+++
@@ -1,5 +0,0 @@
-package com.nhnacademy.gatewayapi.domain.exception;
-
-public class UserNotFoundException extends RuntimeException {
-
-}
